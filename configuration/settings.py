43691b0327951334b64366e494712d199e6efa3b
Sean Qian <Shengxin.Qian@ibm.com>
2019-08-20 13:18:00 -0400

    add constants for callback and monitor

--- configuration/settings.py
+++ configuration/settings.py
@@ -6,3 +6,14 @@ from pathlib import Path
 PROJECT_ADDR = str(Path(__file__).parent.parent)
 
 RESOURCES_ADDR = PROJECT_ADDR + "/resources/"
+
+class AccuracyStats:
+    LABELS = "labels"
+    PREDICTS = "predicts"
+
+class MonitorMode:
+    TRACK = "track"
+    EVAL = "eval"
+
+class LossStats:
+    LOSS = "loss"
