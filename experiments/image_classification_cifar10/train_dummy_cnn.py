ebbcaba9333adda16c655271376111ddae9705a6
Sean Qian <Shengxin.Qian@ibm.com>
2019-08-22 15:47:58 -0400

    change training dummy cnn on cifar10 with new trainer

--- experiments/image_classification_cifar10/train_dummy_cnn.py
+++ experiments/image_classification_cifar10/train_dummy_cnn.py
@@ -1,5 +1,5 @@
 """
-This module is for training and evaluation of DummyCNN on cifar10 dataset
+This module is for trainer and evaluation of DummyCNN on cifar10 dataset
 """
 import torch
 from torch import nn
@@ -7,13 +7,18 @@ from torch import optim
 import torchvision
 import torchvision.transforms as transforms
 from models.dummynet import dummy_cnn
-from training.basic_training import BasicTraining
+from trainer.basic_trainer import BasicTrainer
+from utils.monitors.cmdline_monitor import CmdLineMonitor
+from utils.monitors.tensorboard_monitor import TensorboardMonitor
+from utils.monitors.callbacks.accuracy_callback import AccuracyCallback
+from utils.monitors.callbacks.loss_callback import LossCallback
+from trainer.deprecation.basic_training import BasicTraining
 
 def perform_experiment():
     """
-    1. Load cifar10 dataset into training dataloader and test dataloader
+    1. Load cifar10 dataset into trainer dataloader and test dataloader
     2. Instantiate ResNet model and BasicTraining class
-    3. Start training and evaluation
+    3. Start trainer and evaluation
     """
     # transform PIL image from [0, 1] to [-1, 1]
     # the first tuple is the means for 3 different channels
@@ -42,17 +47,52 @@ def perform_experiment():
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     cnn = dummy_cnn.DummyCNN().to(device)
-    training = BasicTraining()
-    training.train(cnn
-                   , trainloader
-                   , nn.CrossEntropyLoss()
-                   , optim.SGD(cnn.parameters(), lr=0.01)
-                   , 4
-                   , device
-                   , loss_display_interval=2000
-                   , tensorboardx_loss_display_interval=10
-                   , accuracy_display_batch=2000)
-    training.eval(cnn, testloader, device)
+
+    train_monitors = dict()
+    train_monitors[CmdLineMonitor.__name__] = CmdLineMonitor()
+    train_monitors[CmdLineMonitor.__name__].add_callbacks(AccuracyCallback())
+    train_monitors[CmdLineMonitor.__name__].add_callbacks(LossCallback())
+    train_monitors[TensorboardMonitor.__name__] = TensorboardMonitor(type(cnn).__name__)
+    train_monitors[TensorboardMonitor.__name__].add_callbacks(AccuracyCallback())
+    train_monitors[TensorboardMonitor.__name__].add_callbacks(LossCallback())
+
+    test_monitors = dict()
+    test_monitors[CmdLineMonitor.__name__] = CmdLineMonitor()
+    test_monitors[CmdLineMonitor.__name__].add_callbacks(AccuracyCallback())
+    #It is better to have a different name for TensorboardMonitor because it determine the runs folder
+    # and one run supposed only have one writer (one monitor)
+    test_monitors[TensorboardMonitor.__name__] = TensorboardMonitor(type(cnn).__name__ + "_test")
+    test_monitors[TensorboardMonitor.__name__].add_callbacks(AccuracyCallback())
+
+    trainer = BasicTrainer()
+    trainer.train(cnn
+                  , trainloader
+                  , nn.CrossEntropyLoss()
+                  , optim.SGD(cnn.parameters(), lr=0.01)
+                  , 2
+                  , device
+                  , train_monitors
+                  , checkpoint_name="checkpoint"
+                  , checkpoint_interval=2 * len(trainloader))
+
+    trainer.eval(cnn
+                 , testloader
+                 , device
+                 , test_monitors)
+
+    test_monitors[TensorboardMonitor.__name__].flush()
+
+    # training = BasicTraining()
+    # training.train(cnn
+    #                , trainloader
+    #                , nn.CrossEntropyLoss()
+    #                , optim.SGD(cnn.parameters(), lr=0.01)
+    #                , 4
+    #                , device
+    #                , loss_display_interval=2000
+    #                , tensorboardx_loss_display_interval=10
+    #                , accuracy_display_batch=2000)
+    # training.eval(cnn, testloader, device)
 
 if __name__ == "__main__":
     perform_experiment()
